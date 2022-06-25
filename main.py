26bf54bee8f97bcba315f7e857cbe27a7280b27d
67744833+Haru38@users.noreply.github.com <67744833+Haru38@users.noreply.github.com>
2022-06-26 00:41:34 +0900

    make predict function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 import warnings
 warnings.simplefilter('ignore')
 
-tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, is_fast=True)
+tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, is_fast=True,model_max_length=args.max_input_length)
 trained_model = T5ForConditionalGeneration.from_pretrained(MODEL_DIR)
 cs = CalcSim(trained_model,tokenizer,args.max_input_length,args.max_target_length)
 
